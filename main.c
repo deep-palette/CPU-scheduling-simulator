1f108072fb93da18369da423883d0c9adfb10fb5
Giant-hornet <69618057+Giant-hornet@users.noreply.github.com>
2022-06-17 16:38:49 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "DataStructure.h"
+#include "ProcessSimulator.h"
+
+
+int main()
+{
+	int ProcessCnt, time_quantum;
+	Simulator* simulator[6];
+
+	initSimulator(&simulator[0], FCFS);
+	initSimulator(&simulator[1], SJF);
+	initSimulator(&simulator[2], SRTF);
+	initSimulator(&simulator[3], RR);
+	initSimulator(&simulator[4], NON_PREEMPTIVE_PRIORITY);
+	simulator[5] = NULL;
+
+
+	Simulator_GenerateProcess(simulator, &time_quantum);
+
+	for (int i = 0; i < 5; i++) Simulator_Start(simulator[i], time_quantum);
+
+	Simulator_Terminate(simulator);
+	
+	system("pause");
+	return 0;
+
+
+	
+
+
+}
